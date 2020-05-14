c56b18680f04bd1a7000b2d0ea2c9eb15af1ce12
shlomo <shlomo.gevim@gmail.com>
2020-05-14 08:00:06 +0300

    basic

--- app/src/main/java/com/example/arcore10/MainActivity.kt
+++ app/src/main/java/com/example/arcore10/MainActivity.kt
@@ -1,12 +1,137 @@
 package com.example.arcore10
 
+import android.media.CamcorderProfile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.animation.ModelAnimator
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private lateinit var photoSaver: PhotoSaver
+    private lateinit var videoRecorder: VideoRecorder
+    private var isRecording = false
+
+
+
+      //Original versia of fly
+
+     private val spaceship = Models.Bee
+     private var modelResourceId = R.raw.beedrill
+
+
+      private val nodes = mutableListOf<RotatingNode>()
+      private lateinit var arFragment: ArFragment
+      private var curCameraPosition = Vector3.zero()
+
+
+       override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+          setContentView(R.layout.activity_main)
+          arFragment = fragment as ArFragment
+          arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+              loadModelAndAddToScene(hitResult.createAnchor(), modelResourceId)
+          }
+          arFragment.arSceneView.scene.addOnUpdateListener {
+              updateNodes()
+          }
+          setupFab()
+      }
+
+    private fun loadModelAndAddToScene(anchor: Anchor, modelResourceId: Int) {
+        ModelRenderable.builder()
+            .setSource(this, modelResourceId)
+            .build()
+            .thenAccept { modelRenderable ->
+                addNodeToScene(anchor, modelRenderable, spaceship)
+                eliminateDot()
+            }.exceptionally {
+                Toast.makeText(this, "Error creating node: $it", Toast.LENGTH_LONG).show()
+                null
+            }
+
     }
-}
+
+    private fun updateNodes() {
+        curCameraPosition = arFragment.arSceneView.scene.camera.worldPosition
+        for (node in nodes) {
+            node.worldPosition =
+                Vector3(curCameraPosition.x, node.worldPosition.y, curCameraPosition.z)
+        }
+    }
+
+    private fun addNodeToScene(
+        anchor: Anchor,
+        modelRenderable: ModelRenderable,
+        spaceship: Models
+    ) {
+        val anchorNode = AnchorNode(anchor)
+        val rotatingNode = RotatingNode(spaceship.degreesPerSecond).apply {
+            setParent(anchorNode)
+        }
+        Node().apply {
+            renderable = modelRenderable
+            setParent(rotatingNode)
+            localPosition = Vector3(spaceship.radius, spaceship.height, 0f)
+            localRotation = Quaternion.eulerAngles(Vector3(0f, spaceship.rotationDegrees, 0f))
+        }
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        nodes.add(rotatingNode)
+        val animationData = modelRenderable.getAnimationData("Betta_Splendens")
+        ModelAnimator(animationData, modelRenderable).apply {
+            repeatCount = ModelAnimator.INFINITE
+            start()
+        }
+    }
+    private fun eliminateDot() {
+        arFragment.arSceneView.planeRenderer.isVisible = false
+        arFragment.planeDiscoveryController.hide()
+        arFragment.planeDiscoveryController.setInstructionView(null)
+    }
+
+    private fun setupFab() {
+        photoSaver = PhotoSaver(this)
+        videoRecorder = VideoRecorder(this).apply {
+            sceneView = arFragment.arSceneView
+
+            setVideoQuality(CamcorderProfile.QUALITY_1080P, resources.configuration.orientation)
+        }
+        fab.setOnClickListener {
+            if (!isRecording) {
+                photoSaver.takePhoto(arFragment.arSceneView)
+            }
+        }
+
+        fab.setOnLongClickListener {
+            isRecording = videoRecorder.toggleRecordingState()
+            true
+        }
+
+        fab.setOnTouchListener { view, motionEvent ->
+
+            if (motionEvent.action == MotionEvent.ACTION_UP && isRecording) {
+
+                isRecording = videoRecorder.toggleRecordingState()
+
+                Toast.makeText(this, "Saved video to gallery!", Toast.LENGTH_LONG).show()
+
+                true
+
+            } else false
+
+        }
+
+    }
+  }
+
+
+
